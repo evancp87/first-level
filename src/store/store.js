ccb811c7487ae940883097ccde0b81de2231bcc8
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-06-18 14:40:58 +0100

    minor config changes

--- src/store/store.js
+++ src/store/store.js
@@ -1,20 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import gameReducer from "../features/Game/GameSlice";
+import gameReducer from "../features/Game/GameSlice";
 // import searchReducer from "../features/search/searchSlice";
 // import cartReducer from "../features/cart/cartSlice";
 // import favoritesReducer from "../features/favorites/favoritesSlice";
 import dashboardReducer from "../features/Dashboard/dashboardSlice";
 import logger from "redux-logger";
-
+import thunk from "redux-thunk";
 export const store = configureStore({
   reducer: {
-    // games: gameReducer,
+    games: gameReducer,
     // cart: cartReducer,
     dashboard: dashboardReducer,
     // favorites: favoritesReducer,
     // search: searchReducer,
   },
-
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ serializableCheck: false }).concat(logger),
+  middleware: [thunk, logger],
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware({ serializableCheck: false }).concat(logger, thunk),
 });
