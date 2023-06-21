347759d651b3b6000ffe1c8abb74240ab9d5e009
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-06-21 21:34:03 +0100

    handling toggling of likes- not 100% working

--- src/features/Game/GameCard.jsx
+++ src/features/Game/GameCard.jsx
@@ -8,6 +8,9 @@ import { useLocalStorage } from "../../utils/hooks/localStorage";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import { useDispatch, useSelector } from "react-redux";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import {
   gameLikes,
   addToLikes,
@@ -16,56 +19,90 @@ import {
 
 const GameCard = ({ game }) => {
   //   if in basket show market
-  const { released, name, background_image, slug, id, rating, platforms } =
-    game;
+  const {
+    released,
+    name,
+    background_image,
+    slug,
+    id,
+    rating,
+    platforms,
+    // genres,
+    liked,
+    // rating: {
+    //   ratings: [
+    //     { id: ratingId },
+    //     { title: ratingTitle },
+    //     { count: ratingCount },
+    //     { percent: ratingPercent },
+    //   ],
+    // },
+  } = game;
 
   const dispatch = useDispatch();
-
+  // const notify = () => toast("Added to favorites!");
   const [toggleLikes, setToggleLikes] = useLocalStorage("Likes", []);
 
   const handleLikes = (id, game) => {
     dispatch(gameLikes(id));
-
+    // console.log(Object.entries(toggleLikes));
     setToggleLikes(game);
+    //
   };
   const platformNames = platforms.map((console) => console.platform.name);
 
-  const liked = game.liked;
+  // const ratingsArray = rating.map((rating) => rating.id);
+  // const liked = game.liked;
 
   return (
     <>
-      <article className="card card-bordered">
-        <div className="card-body">
-          <Link to={`/game/${slug}`}>
-            <img
-              style={{ width: "30%", height: "30%" }}
+      <SkeletonTheme color="#f3f3f3" highlightColor="#e0e0e0">
+        <article className="card card-side items-center flex-row bg-base-100 shadow-xl card-bordered m-6 mx-w-full flex-wrap">
+          <picture className="sm:max-w-[100%] lg:max-w-[30%] rounded-sm">
+            <Link to={`/game/${slug}`}>
+              {/* <img
+              className="rounded-xl"
+              // style={{ width: "30%", height: "30%" }}
               src={background_image}
               alt={name}
-            />
-          </Link>
+            /> */}
+              {background_image ? (
+                <img
+                  className="rounded-xl"
+                  // style={{ width: "30%", height: "30%" }}
+                  src={background_image}
+                  alt={name}
+                />
+              ) : (
+                <Skeleton width={100} count={3} />
+              )}
+            </Link>
+          </picture>
 
-          <div>
-            <h3 className="card-title">{name}</h3>
-            <p>Released {released}</p>
-            <p>{platformNames}</p>
+          <div className="card-body">
+            <div>
+              <h2 className="card-title ">{name}</h2>
+              <p>Released {released}</p>
+              <p>{platformNames}</p>
+            </div>
+            <div>
+              <FontAwesomeIcon
+                style={{
+                  filter: liked
+                    ? "brightness(0) saturate(100%) invert(23%) sepia(80%) saturate(6831%) hue-rotate(355deg) brightness(98%) contrast(123%)"
+                    : "filter: brightness(0) saturate(100%) invert(100%) sepia(3%) saturate(7500%) hue-rotate(296deg) brightness(127%) contrast(115%)",
+                }}
+                icon={faHeart}
+                onClick={() => {
+                  // setLikes([...likes, game]);
+                  handleLikes(game.id, game);
+                }}
+              />
+              <p className="card-actions justify-end">{rating}</p>
+            </div>
           </div>
-          <div>
-            <FontAwesomeIcon
-              style={{
-                filter: liked
-                  ? "brightness(0) saturate(100%) invert(23%) sepia(80%) saturate(6831%) hue-rotate(355deg) brightness(98%) contrast(123%)"
-                  : "filter: brightness(0) saturate(100%) invert(100%) sepia(3%) saturate(7500%) hue-rotate(296deg) brightness(127%) contrast(115%)",
-              }}
-              icon={faHeart}
-              onClick={() => {
-                // setLikes([...likes, game]);
-                handleLikes(game.id, game);
-              }}
-            />
-            <p>{rating}</p>
-          </div>
-        </div>
-      </article>
+        </article>
+      </SkeletonTheme>
     </>
   );
 };
