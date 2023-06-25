2335afbc5f8627c91d8ea3663777caa0b38f105a
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-06-25 15:48:43 +0100

    page transition animations implemented

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,43 @@
 import { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Nav from "./Nav";
+
+import { Transition, SwitchTransition } from "react-transition-group";
+import { onPageEnter, onPageExit } from "../utils/helpers";
+
 const Layout = ({ children }) => {
+  const [isTransitioning, setIsTransitioning] = useState(false);
+
+  const handleEnter = (node) => {
+    onPageEnter(node);
+    setIsTransitioning(false);
+  };
+
+  const handleExit = (node) => {
+    onPageExit(node);
+    setIsTransitioning(true);
+  };
+
+  const location = useLocation();
+
   return (
     <>
       <Nav />
-      <main className="flex flex-col">
-        <Outlet />
-        {children}
-      </main>
+      <SwitchTransition mode="out-in">
+        <Transition
+          key={location.pathname}
+          classNames="page"
+          // timeout={200}
+          onEnter={handleEnter}
+          onExit={handleExit}
+          unmountOnExit
+        >
+          <main className="flex flex-col">
+            <Outlet />
+            {children}
+          </main>
+        </Transition>
+      </SwitchTransition>
     </>
   );
 };
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,4 @@
+import { gsap } from "gsap";
 // For truncating strings where needed
 export const truncateText = (str, num) => {
   if (str.length <= num) {
@@ -18,3 +19,85 @@ export const sumItems = (items) => {
 
   return { itemCount, total };
 };
+
+export const calculateOverallRating = (ratingData) => {
+  let sum = 0;
+  let total = 0;
+  ratingData.ratings.forEach((rating) => {
+    sum += rating.title * rating.count;
+    total += rating.count;
+  });
+  return total > 0 ? sum / total : 0;
+};
+
+export const onPageEnter = (node) => {
+  gsap.from(node, {
+    y: 50,
+    delay: 0.2,
+    ease: "power3",
+    opacity: 0,
+    stagger: {
+      amount: 0.2,
+    },
+  });
+};
+
+export const onPageExit = (node) => {
+  gsap.to(node, {
+    delay: 0.2,
+    ease: "power3",
+    opacity: 0,
+    stagger: {
+      amount: 0.2,
+    },
+  });
+};
+
+// export const heartGenerator = (node) => {
+//   gsap.to(element, {
+//     duration: 2,
+//     physics2D: {
+//       velocity: 300,
+//       angle: -60,
+//       acceleration: 50,
+//       accelerationAngle: 180,
+//     },
+//   });
+// };
+
+// let dots = [],
+//   bg = document.querySelector("#featureBackground"),
+//   i, dot;
+
+// // create 80 dot elements and put them in an array
+// for (i = 0; i < 80; i++) {
+//   dot = document.createElement("div");
+//   dot.setAttribute("class", "dot");
+//   bg.appendChild(dot);
+//   dots.push(dot);
+// }
+
+// //set the initial position of all the dots, and pick a random color for each from an array of colors
+// gsap.set(dots, {
+//   backgroundColor: "random([#663399,#84d100,#cc9900,#0066cc,#993333])",
+//   scale: "random(0.4, 1)",
+//   x:400,
+//   y:300
+// });
+
+// // create the physics2D animation
+// let tween = gsap.to(dots, {
+//     duration: 2.5,
+//     physics2D: {
+//       velocity: "random(200, 650)",
+//       angle: "random(250, 290)",
+//       gravity: 500
+//     },
+//     delay: "random(0, 2.5)"
+//   });
+
+// GSDevTools.create({
+//   animation: tween,
+//   container: "#featureAnimation",
+//   minimal: true
+// });
