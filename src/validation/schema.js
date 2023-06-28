1f5638c1c486e84b8d7f0e7a81b7b4c3d7ffe000
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-06-28 19:51:30 +0100

    tweak to joi schema

--- src/validation/schema.js
+++ src/validation/schema.js
@@ -1,4 +1,4 @@
 import joi from "joi";
 export const searchSchema = {
-  search: joi.string().trim().min(3).required().label("Search"),
+  search: joi.string().trim().min(2).required().label("Search"),
 };
