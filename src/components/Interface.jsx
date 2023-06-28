df399390ce0870e9f5fc36439ac00474d6e5ea4a
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-06-28 20:03:51 +0100

    tweak to imports

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Dashboard from "../features/Dashboard/Dashboard";
 import Nav from "./Nav";
-import SecondaryNav from "./SecondaryNav";
 // import Dashboard from "../features/Dashboard/Dashboard";
 
 const Interface = () => {
