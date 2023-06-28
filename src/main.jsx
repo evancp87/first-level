9b9b4bde1cd35fb55f71c3fa1ec477b07dd3b8f9
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-06-28 19:52:14 +0100

    removed index.css and unwanted styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import { store } from "./store/store.js";
-import "./index.css";
+// import "./index.css";
 import "../dist/output.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
