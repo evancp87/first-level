7acdb34ff8d7112cbdb5724f9841cb702a83f6da
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-06-28 20:07:42 +0100

    chore: removed unused code & tidying

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState, useCallback, useEffect, lazy, suspense } from "react";
-import { useDispatch, useSelector } from "react-redux";
-// import "./App.css";
-// import "dist/output.css";
+import { useState, useCallback, useEffect } from "react";
+import { useDispatch } from "react-redux";
+
 import "../dist/output.css";
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Search from "./features/Dashboard/Search";
 import Dashboard from "./features/Dashboard/Dashboard";
 import Favorites from "./features/Favorites/Favorites";
 import GameDetail from "./features/Game/GameDetail";
-// import Layout from "./components/Layout";
 import SplashScreen from "./components/SplashScreen";
 import Interface from "./components/Interface";
 import Error404 from "./components/Error404";
@@ -46,8 +44,6 @@ const App = () => {
         <>
           <Layout>
             <Routes>
-              {/* <Route element={<Layout />} /> */}
-
               <Route exact path="/" element={<Interface />} />
               <Route index element={<Dashboard />} />
               <Route path="/search" element={<Search />} />
