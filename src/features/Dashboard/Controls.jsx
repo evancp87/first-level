142a13961efbe297e5f03b5fc742ddb427dd69a7
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-07-01 22:32:38 +0100

    select input reset implemented

--- src/features/Dashboard/Controls.jsx
+++ src/features/Dashboard/Controls.jsx
@@ -1,3 +1,7 @@
+/* eslint-disable react/prop-types */
+
+import React from "react";
+
 const Controls = ({
   sortValue,
   searchError,
@@ -7,14 +11,29 @@ const Controls = ({
   setSelectedGenre,
   platformNames,
   genreNames,
+  selectedGenre,
+  selectedPlatform,
+  setSelectedSort,
+  selectedSort,
+  searchText,
 }) => {
+  // const handleAscendingReset = (e) => {
+  //   setSelectedSort("Sort A - Z");
+  // }
+
+  // const handleGenreResets = (e) => {
+  //   setSelectedGenre(e.target.value)
+  //   setSelectedSort(" Filter games by genre");
+  // }
   return (
     <>
-      <div className="flex flex-col items-center">
-        <div className="flex flex-row flex-wrap justify-center my-px gap-[0.5rem]">
+      <div className="flex flex-col ">
+        <div className="my-1.5 flex flex-row flex-wrap justify-center ">
           <select
             onInput={sortValue}
-            className="select w-[250px] max-w-xs  select-bordered select-xs  max-w-xs"
+            value={selectedSort}
+            // onChange={(e) => setSelectedSort(e.target.value)}
+            className="max-w-xs max-w-xs select-bordered  select select-xs my-[1.5em] w-[250px]"
           >
             <option disabled selected>
               Sort A - Z
@@ -25,8 +44,9 @@ const Controls = ({
           </select>
           <select
             name="platform"
+            value={selectedPlatform}
             onChange={(e) => setSelectedPlatform(e.target.value)}
-            className="select w-[250px] max-w-xs  select-bordered select-xs  max-w-xs"
+            className="max-w-xs max-w-xs select-bordered  select select-xs my-[1.5em] w-[250px]"
           >
             <option disabled selected>
               Filter games by console
@@ -47,8 +67,9 @@ const Controls = ({
           </select>
           <select
             name="genre"
+            value={selectedGenre}
             onChange={(e) => setSelectedGenre(e.target.value)}
-            className="select w-[250px] max-w-xs  select-bordered select-xs  max-w-xs"
+            className="max-w-xs max-w-xs select-bordered  select select-xs my-[1.5em] w-[250px]"
           >
             <option disabled selected>
               Filter games by genre
@@ -64,23 +85,30 @@ const Controls = ({
             </option> */}
           </select>
         </div>
-        <div className="form-control flex justify-center max-w-[80vw] w-full">
+        <div className="form-control  flex w-full  max-w-[80vw] justify-center">
           <label className="label">
             <span className="label-text">Search for games</span>
           </label>
           <input
             onChange={searchValue}
+            value={searchText}
             type="text"
             placeholder="Search for games by title, genre or platform"
-            className="input input-bordered w-full max-w-xs"
+            className="max-w-xs input-bordered input my-[1.5em] w-full"
           />
           <ul>
             {searchError &&
               searchError.map((error, index) => (
-                <li key={index}>{error.message}</li>
+                <li key={index}>
+                  <p className="mb-4 text-logo"> {error.message}</p>
+                </li>
               ))}
           </ul>
-          <button value="reset" onClick={resetFilters}>
+          <button
+            value="reset"
+            className="text-slate-100 h-[40px] max-w-[25%] rounded-full bg-logo"
+            onClick={resetFilters}
+          >
             Reset
           </button>
         </div>
