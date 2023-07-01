1c10c6cf24fe9e192ad609ba2e15fbdb8bcac4c8
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-07-01 22:12:49 +0100

    picture of 404 & styling

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,10 +1,24 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Error404 = () => {
   return (
-    <>
-      <p>Not found</p>
-    </>
+    <div className="jusitfy-center flex flex-col items-center">
+      <div className="flex flex-row justify-between">
+        <Link to="/">Home</Link>
+        <h2>Not found</h2>
+      </div>
+      <img
+        className="rounded-md"
+        src="src/assets/empty.png"
+        alt="empty chest"
+      />
+      <p>
+        The page could not be found, please check the address bar for typos, and
+        make sure the page exists, otherwise follow the link back to the home
+        page
+      </p>
+    </div>
   );
 };
 
