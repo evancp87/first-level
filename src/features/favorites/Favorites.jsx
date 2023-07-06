f07d5786a56b2b8f173ea76279aa02871325d362
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-07-06 13:26:41 +0100

    changed likes value and added gsap animation that isn't working yet

--- src/features/favorites/Favorites.jsx
+++ src/features/favorites/Favorites.jsx
@@ -1,39 +1,55 @@
 import React, { useState } from "react";
-// import { useLocalStorage } from "@uidotdev/usehooks";
 import GameCard from "../Game/GameCard";
-import { removeLike, getLikes, selectLikes } from "../Dashboard/dashboardSlice";
-import { useDispatch, useSelector } from "react-redux";
-import { useLocalStorage } from "../../utils/hooks/localStorage";
 import { useHandleLikes } from "../../utils/hooks/localStorage.jsx";
-
+import gsap from "gsap";
+import { Transition } from "react-transition-group";
 const Favorites = () => {
-  //  use useLocalStorage hooks here to loop over keys of localStorage and show game cards
-  // const likesFromState = useSelector(selectLikes);
-  // console.log(likesFromState);
-  // const [...localStorageLikes] = useLocalStorage("Likes", []);
-  // const likes = [...likesFromState, ...localStorageLikes];
-  const [likesItem] = useLocalStorage("Likes", []);
   const { likes, handleLikes } = useHandleLikes();
+  const [isTransitioning, setIsTransitioning] = useState(false);
+
+  const onLikeExit = (node) => {
+    gsap.to(node, {
+      x: -100,
+      delay: 0.2,
+      ease: "power3",
+      opacity: 0,
+      stagger: {
+        amount: 0.2,
+      },
+    });
+  };
 
   return (
-    <ul>
-      {(likesItem.length === 0 && (
-        <p className="flex items-center justify-center text-xl">
-          Nothing here. Like Games to add to this section
-        </p>
-      )) ||
-        []}
-      {likesItem &&
-        likesItem.map((game) => (
-          <li key={game.id}>
-            <GameCard
-              game={game}
-              liked={game.liked}
-              handleLikes={handleLikes}
-            />
-          </li>
-        ))}
-    </ul>
+    <div className="h-screen">
+      <ul>
+        {(likes.length === 0 && (
+          <p className="mt-8 flex items-center justify-center text-xl">
+            Nothing here. Like games to add to this section
+          </p>
+        )) ||
+          []}
+        {/* <TransitionGroup> */}
+        {likes &&
+          likes.map((game) => (
+            <Transition
+              key={game.id}
+              timeout={200}
+              in={true}
+              // onEnter={handleEnter}
+              onExit={onLikeExit}
+              unmountOnExit
+            >
+              <li key={game.id}>
+                <GameCard
+                  game={game}
+                  liked={game.liked}
+                  handleLikes={handleLikes}
+                />
+              </li>
+            </Transition>
+          ))}
+      </ul>
+    </div>
   );
 };
 
