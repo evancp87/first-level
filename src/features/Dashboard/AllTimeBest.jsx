b300f7402f1ba2c0ddedaf6c70cbac9cdb444295
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-07-06 14:00:36 +0100

    added heading

--- src/features/Dashboard/AllTimeBest.jsx
+++ src/features/Dashboard/AllTimeBest.jsx
@@ -14,17 +14,21 @@ const AllTimeBest = () => {
 
   const highestRated = useSelector(selectHighestRated);
   return (
-    <>
-      {highestRated &&
-        highestRated.map((game) => (
-          <GameCard
-            key={game.id}
-            game={game}
-            liked={game.liked}
-            handleLikes={handleLikes}
-          />
-        ))}
-    </>
+    <div className="p-4">
+      <h2 className="my-4">All Time Highest Rated</h2>
+      <ul>
+        {highestRated &&
+          highestRated.map((game) => (
+            <li key={game.id}>
+              <GameCard
+                game={game}
+                liked={game.liked}
+                handleLikes={handleLikes}
+              />
+            </li>
+          ))}
+      </ul>
+    </div>
   );
 };
 
