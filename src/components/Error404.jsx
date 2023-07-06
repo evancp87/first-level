790eb8350a8e5eafb6b91ce32b905163e113f77d
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-07-06 13:43:42 +0100

    error page built out and styled

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,23 +1,26 @@
-import { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
 const Error404 = () => {
   return (
-    <div className="jusitfy-center flex flex-col items-center">
-      <div className="flex flex-row justify-between">
-        <Link to="/">Home</Link>
-        <h2>Not found</h2>
-      </div>
+    <div className="flex flex-col items-center justify-center">
+      <h2 className="my-4">Not found</h2>
+
       <img
-        className="rounded-md"
+        className="my-4 aspect-[3/3] rounded-md"
         src="src/assets/empty.png"
         alt="empty chest"
       />
-      <p>
-        The page could not be found, please check the address bar for typos, and
-        make sure the page exists, otherwise follow the link back to the home
-        page
-      </p>
+      <div className="flex w-[30%] flex-col items-center">
+        <p>
+          The page could not be found, please check the address bar for typos,
+          and make sure the page exists, otherwise follow the link back to the
+          home page
+        </p>
+        <button className="active-btn text-slate-100 my-4 h-[40px] w-[200px]  rounded-full bg-logo">
+          <Link to="/">Home</Link>
+        </button>
+      </div>
     </div>
   );
 };
