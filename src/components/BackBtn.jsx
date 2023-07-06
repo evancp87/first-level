5930ed5343056545e718777b2d3fda8c8db89219
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-07-06 13:35:18 +0100

    backbtn implement

--- src/components/BackBtn.jsx
+++ src/components/BackBtn.jsx
@@ -1,10 +1,23 @@
-import { useState } from "react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 const BackBtn = () => {
+  const navigate = useNavigate();
   return (
     <>
-      <FontAwesomeIcon icon={faArrowLeft} />
+      <FontAwesomeIcon
+        onClick={() => {
+          navigate(-1);
+        }}
+        className="absolute left-5 top-5 z-40 cursor-pointer"
+        style={{
+          filter:
+            "brightness(0) saturate(100%) invert(49%) sepia(93%) saturate(395%) hue-rotate(82deg) brightness(87%) contrast(86%)",
+        }}
+        size="2x"
+        icon={faArrowLeft}
+      />
     </>
   );
 };
