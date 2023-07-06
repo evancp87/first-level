25de9f7c3fb339f1051af1ab3a2a3294cc6b5e7c
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-07-06 13:59:09 +0100

    made selects controlled components

--- src/features/Dashboard/Controls.jsx
+++ src/features/Dashboard/Controls.jsx
@@ -17,30 +17,19 @@ const Controls = ({
   selectedSort,
   searchText,
 }) => {
-  // const handleAscendingReset = (e) => {
-  //   setSelectedSort("Sort A - Z");
-  // }
-
-  // const handleGenreResets = (e) => {
-  //   setSelectedGenre(e.target.value)
-  //   setSelectedSort(" Filter games by genre");
-  // }
   return (
     <>
-      <div className="flex flex-col ">
+      <div className="flex max-w-[80vw] flex-col  items-center">
         <div className="my-1.5 flex flex-row flex-wrap justify-center ">
           <select
             onInput={sortValue}
             value={selectedSort}
-            // onChange={(e) => setSelectedSort(e.target.value)}
+            onChange={(e) => setSelectedSort(e.target.value)}
             className="max-w-xs max-w-xs select-bordered  select select-xs my-[1.5em] w-[250px]"
           >
-            <option disabled selected>
-              Sort A - Z
-            </option>
+            <option disabled>Sort A - Z</option>
             <option value="Asc">Asc</option>
             <option value="Desc">Desc</option>
-            {/* <option value="Reset">Reset</option> */}
           </select>
           <select
             name="platform"
@@ -48,22 +37,13 @@ const Controls = ({
             onChange={(e) => setSelectedPlatform(e.target.value)}
             className="max-w-xs max-w-xs select-bordered  select select-xs my-[1.5em] w-[250px]"
           >
-            <option disabled selected>
-              Filter games by console
-            </option>
+            <option disabled>Filter games by console</option>
             {platformNames &&
               platformNames.map((platformName, index) => (
-                <option
-                  key={index}
-                  // value="Asc"
-                  value={platformName}
-                >
+                <option key={index} value={platformName}>
                   {platformName}
                 </option>
               ))}
-            {/* <option onChange={resetFilters} value="Reset">
-              Reset
-            </option> */}
           </select>
           <select
             name="genre"
@@ -71,22 +51,17 @@ const Controls = ({
             onChange={(e) => setSelectedGenre(e.target.value)}
             className="max-w-xs max-w-xs select-bordered  select select-xs my-[1.5em] w-[250px]"
           >
-            <option disabled selected>
-              Filter games by genre
-            </option>
+            <option disabled>Filter games by genre</option>
             {genreNames &&
               genreNames.map((genreName, index) => (
                 <option key={index} value={genreName}>
                   {genreName}
                 </option>
               ))}
-            {/* <option onChange={resetFilters} value="Reset">
-              Reset
-            </option> */}
           </select>
         </div>
-        <div className="form-control  flex w-full  max-w-[80vw] justify-center">
-          <label className="label">
+        <div className="form-control  flex w-full justify-center">
+          <label className="label self-start">
             <span className="label-text">Search for games</span>
           </label>
           <input
@@ -94,7 +69,7 @@ const Controls = ({
             value={searchText}
             type="text"
             placeholder="Search for games by title, genre or platform"
-            className="max-w-xs input-bordered input my-[1.5em] w-full"
+            className="focus-input max-w-xs input-bordered input my-[1.5em] w-full"
           />
           <ul>
             {searchError &&
@@ -106,7 +81,7 @@ const Controls = ({
           </ul>
           <button
             value="reset"
-            className="text-slate-100 h-[40px] max-w-[25%] rounded-full bg-logo"
+            className="active-btn text-slate-100 h-[40px] w-[25%] self-start rounded-full bg-logo duration-300 ease-in-out hover:scale-110"
             onClick={resetFilters}
           >
             Reset
