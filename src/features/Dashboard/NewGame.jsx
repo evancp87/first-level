71b37b8379476bc81db2e9946a071648ac185a9e
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-07-07 15:23:46 +0100

    tailwind utility classes tidy

--- src/features/Dashboard/NewGame.jsx
+++ src/features/Dashboard/NewGame.jsx
@@ -1,43 +1,45 @@
 /* eslint-disable react/prop-types */
 import React from "react";
-import Skeleton from "react-loading-skeleton";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import { Link } from "react-router-dom";
 const FilteredGameCard = ({ game }) => {
   const { released, name, background_image, rating, slug } = game;
 
   return (
-    <div className="w-full overflow-x-auto">
-      {/* links to game detail */}
-      <Link to={`/game/${slug}`}>
-        <div
-          className="carousel-item image-full relative h-[300px] w-[200px] gap-[1em] rounded-3xl bg-base-100 shadow-xl md:w-96  "
-          style={{
-            backgroundImage: background_image ? (
-              `url(${background_image})`
-            ) : (
-              <Skeleton count={1} />
-            ),
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-            backgroundBlendMode: "multiply",
-            backgroundColor: "rgb(97 97 97 / 50%)",
-          }}
-        >
+    <SkeletonTheme color="#f3f3f3" highlightColor="#e0e0e0">
+      <div className="w-full overflow-x-auto">
+        {/* links to game detail */}
+        <Link to={`/game/${slug}`}>
           <div
-            className=" bg-blue-400 relative flex flex-col justify-start justify-center gap-[0.5em] ps-[1em] pt-[1em]  
-          "
+            className="carousel-item image-full relative h-[300px] w-[200px] gap-[1em] rounded-3xl bg-base-100 shadow-xl md:w-96  "
+            style={{
+              backgroundImage: background_image ? (
+                `url(${background_image})`
+              ) : (
+                <Skeleton count={1} />
+              ),
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+              backgroundBlendMode: "multiply",
+              backgroundColor: "rgb(97 97 97 / 50%)",
+            }}
           >
-            <h2>{name || <Skeleton count={1} />}</h2>
-            <p>{`Released ${released}` || <Skeleton count={1} />}</p>
-            <div className=" justify-end">
-              <p>{`Rating: ${rating}` || <Skeleton count={1} />}</p>
+            <div
+              className=" bg-blue-400 relative flex flex-col justify-start justify-center gap-[0.5em] ps-[1em] pt-[1em]  
+          "
+            >
+              <h2>{name || <Skeleton count={1} />}</h2>
+              <p>{`Released ${released}` || <Skeleton count={1} />}</p>
+              <div className=" justify-end">
+                <p>{`Rating: ${rating}` || <Skeleton count={1} />}</p>
+              </div>
             </div>
           </div>
-        </div>
-      </Link>
-    </div>
+        </Link>
+      </div>
+    </SkeletonTheme>
   );
 };
 
