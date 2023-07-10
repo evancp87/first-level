85312262abac719a2723cd8ee3e8d78f4018db5a
Evan Parker <65721884+evancp87@users.noreply.github.com>
2023-07-10 14:29:43 +0100

    change to css file path

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import { store } from "./store/store.js";
-import "../css/output.css";
+// import "../css/output.css";
+import "../dist/assets/output.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
